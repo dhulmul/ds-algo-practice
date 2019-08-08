bb53f3dcb12ef91ee0c70b85887de5aa55d66c4f
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-08 11:24:24 +0200

    code for binary-tree-inorder-traversal

---
+++ binary-tree-inorder-traversal.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    vector<int> inorderVector;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == NULL) {
+            return inorderVector;
+        }
+        inorderTraversal(root->left);
+        inorderVector.push_back(root->val);
+        inorderTraversal(root->right);
+        return inorderVector;
+    }
+};
